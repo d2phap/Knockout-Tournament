b3bd3ed25fe4ea45f1f34d038a2194118f4687f0
Phap Dieu Duong <phap@imageglass.org>
2017-02-12 19:01:08 +0800

    add shading effect to the match UI

--- client/app/gameConfig.js
+++ client/app/gameConfig.js
@@ -1,6 +1,6 @@
 
-const GAME_SERVER_URL = "http://localhost:9876/"; // for selenium server
-// const GAME_SERVER_URL = "http://localhost:8765/"; // for game server
+// const GAME_SERVER_URL = "http://localhost:9876/"; // for selenium server
+const GAME_SERVER_URL = "http://localhost:8765/"; // for game server
 
 var exports = exports || null;
 if (exports) {
--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -553,8 +553,8 @@ module.exports = { RoundController };
 /***/ (function(module, exports) {
 
 
-const GAME_SERVER_URL = "http://localhost:9876/"; // for selenium server
-// const GAME_SERVER_URL = "http://localhost:8765/"; // for game server
+// const GAME_SERVER_URL = "http://localhost:9876/"; // for selenium server
+const GAME_SERVER_URL = "http://localhost:8765/"; // for game server
 
 var exports = exports || null;
 if (exports) {
